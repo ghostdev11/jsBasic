1273a7039c7fa67ca8686d31158d4c1f1a08bd07
Ghost Dev <ntrongquy2003@gmail.com>
2024-08-06 15:05:17 +0700

    video7 chapter2 bai3

---
+++ video7/video7.js
@@ -0,0 +1,8 @@
+console.log(' hello word')
+// biến có thể tùy chỉnh
+let x=5; 
+let y=6;
+console.log(`hello word from javascript:`, 'x=', x, 'y=', y);
+// biến cố định
+const z=8;
+console.log('kq const: ', z);
